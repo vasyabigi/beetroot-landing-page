a93be9aeb85351f5c9437ae77004eebc5cb38b7c
Vasyl Stanislavchuk <vasyl.stanislavchuk@gmail.com>
2020-01-28 20:35:06 +0200

    Updated ALLOWED_HOSTS.

--- landing_page_app/settings.py
+++ landing_page_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+4%fjs!=c5ej+=y5tr*bb-^^hao99ck-@bx*h62j+algs^^*5h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["landing-dev.empsrtxdpp.eu-central-1.elasticbeanstalk.com"]
 
 
 # Application definition
